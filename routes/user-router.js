3e270aea8ecb613dc2ff4ace724182b7e498086b
Eak <70291256+hs4qnz@users.noreply.github.com>
2024-04-17 00:28:28 +0700

    Update authorization to user-router.js

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,23 +1,24 @@
 const router = require('express').Router();
 const queryToMongoose = require('../core/query-to-mongoose');
 const userController = require('../controllers/user-controller');
+const auth = require('../middleware/auth'); // Add authorization 
 
 // POST localhost:3000/admin/api/v1/users
-router.post('/admin/api/v1/users', userController.postUser);
+router.post('/admin/api/v1/users', auth, userController.postUser);
 
 // POST localhost:3000/admin/api/v1/users/login
 router.post('/admin/api/v1/users/login', userController.postLogin);
 
 // GET localhost:3000/admin/api/v1/users
-router.get('/admin/api/v1/users', queryToMongoose, userController.getUsers);
+router.get('/admin/api/v1/users', queryToMongoose, auth, userController.getUsers);
 
 // GET localhost:3000/admin/api/v1/users/661c0414b45d61f0c2f7a5c2
-router.get('/admin/api/v1/users/:userId', userController.getUser);
+router.get('/admin/api/v1/users/:userId', auth, userController.getUser);
 
 // PUT localhost:3000/admin/api/v1/users/661c0414b45d61f0c2f7a5c2
-router.put('/admin/api/v1/users/:userId', userController.putUser);
+router.put('/admin/api/v1/users/:userId', auth, userController.putUser);
 
 // DELETE localhost:3000/admin/api/v1/users/661c04568d00a8be547fc34b
-router.delete('/admin/api/v1/users/:userId', userController.deleteUser);
+router.delete('/admin/api/v1/users/:userId', auth, userController.deleteUser);
 
 module.exports = router;
